65b0e8bc38ff592f959002303fc9ee7e7df221cd
Lukas Wilkeer <lukasc4l@gmail.com>
2013-10-08 20:16:17 -0300

    Add a dev user for database access.

--- project/models/Question.py
+++ project/models/Question.py
@@ -6,8 +6,8 @@ class Question(object):
     def __init__(self):
         global db 
         global mongo
-        mongo = Connection('mongodb://<user>:<password>@dharma.mongohq.com:10046/myapp')
-        db = mongo['myapp']
+        mongo = Connection('mongodb://akademia-dev:akademia-dev@paulo.mongohq.com:10084/akademia')
+        db = mongo['akademia']
    
     def get_latest_questions(questions_title):
         questions = db.questions.find().limit(5)
